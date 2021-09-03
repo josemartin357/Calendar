95c3491495fe4a52b945e58f2d2947d82c8fb666
josemartin357 <83382332+josemartin357@users.noreply.github.com>
2021-09-02 23:59:53 -0500

    Update script.js
    2nd commit --> worked on function for block times; pending: localstorage

--- Assets/script.js
+++ Assets/script.js
@@ -7,21 +7,30 @@ var currentTime = moment();
 $("#currentDay").text(currentTime.format("MMM Do, YYYY HH:mm:ss A"));
 
 // WHEN I view the timeblocks for that day ...THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// !!!need if statements to establish function for colors attached to var currentTime
-
+// establishing method to get the hours from the current time
+var currentHour = moment().hour();
 // 9am block
-var nineAm =  
-if (currentTime.isAfter)
+var textAreaElement = $("textarea")
+// console.log(textAreaElement);
+textAreaElement.each(function() {
+console.log($(this).attr("id"));
+
+var timeBlockHour = $(this).attr("id");
+if (currentHour===timeBlockHour) {
+    console.log("present", timeBlockHour);
+    $(this).addClass("present");
+}
+else if (currentHour<timeBlockHour) {
+    console.log("future", timeBlockHour);
+    $(this).addClass("future");
+}
+else {
+    console.log("past", timeBlockHour);
+    $(this).addClass("past");
+}
+})
 
 
-
-// if(date1.getTime() === date2.getTime()){
-//     //same date
-// }
-
-// if(date1.getTime() > date2.getTime()){
-//     //date 1 is newer
-// }
 
 
 // WHEN I click into a timeblock... THEN I can enter an event. WHEN I click the save button for that timeblock ...THEN the text for that event is saved in local storage
