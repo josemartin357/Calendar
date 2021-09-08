b72b6a38cd2b211a744a0275a20b953379401b07
josemartin357 <83382332+josemartin357@users.noreply.github.com>
2021-09-07 21:48:21 -0500

    Update script.js
    updated js with function declaration and other variables for it

--- Assets/script.js
+++ Assets/script.js
@@ -1,42 +1,64 @@
-// LOGIC ROUGH DRAFT
-// note from explanation given by TA grace: DOM traversal, local storage 
 
-// WHEN I open the planner ... THEN the current day is displayed at the top of the calendar
-// establishing currentTime in military time using moment.js doc
+// Establishing currentTime in military time using moment.js doc
 var currentTime = moment();
 $("#currentDay").text(currentTime.format("MMM Do, YYYY HH:mm:ss A"));
 
-// WHEN I view the timeblocks for that day ...THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // establishing method to get the hours from the current time
 var currentHour = moment().hour();
-// 9am block
+// var to grab textarea in the time blocks
 var textAreaElement = $("textarea")
 // console.log(textAreaElement);
-textAreaElement.each(function() {
-console.log($(this).attr("id"));
 
-var timeBlockHour = $(this).attr("id");
-if (currentHour===timeBlockHour) {
-    console.log("present", timeBlockHour);
-    $(this).addClass("present");
-}
-else if (currentHour<timeBlockHour) {
-    console.log("future", timeBlockHour);
-    $(this).addClass("future");
-}
-else {
-    console.log("past", timeBlockHour);
-    $(this).addClass("past");
+
+// -------- ESTABLISHING TIME COMPARISON HERE ------//
+// establishing function for each textAreaElement
+textAreaElement.each(function() {
+    // console.log($(this).attr("id"));
+    // establishing var to grab the ids inside textAreaElement
+    var timeBlockHour = $(this).attr("id");
+    // if the current time is equal to the time in the time block, then....
+    if (currentHour==timeBlockHour) {
+        // console.log("present", timeBlockHour);
+        // ... add style class present
+        $(this).addClass("present");
+    }
+    // if the current time is less than certain block hours, then ...
+    else if (currentHour<timeBlockHour) {
+        // console.log("future", timeBlockHour);
+        // ... those block hours are in the future; add style class for future time
+        $(this).addClass("future");
+    }
+    // if its none of those cases, then its past time ..
+    else {
+        // console.log("past", timeBlockHour);
+        // ... and we add the style class for past
+        $(this).addClass("past");
 }
 })
 
+// -------- ESTABLISHING FUNCTION HERE ------//
+// event specified
+$(".saveBtn").click(function (event) {
+    // prevent default
+    event.preventDefault();
+    // variables to set up local storage
+    // var to grab value of form-control and its siblings
+    var formValue = $(this).siblings(".form-control").val();
+    var keyId = $(this).siblings(".form-control").attr("id");
+    // console.log(keyId);
+    // console.log("this works");
+     // saving to local storage
+    localStorage.setItem(keyId, formValue);    
+});
+
+// for loop
+for (var i = 9; i < 18; i++){
+    console.log("this is i", i);
+    // getting from local storage
+    console.log(localStorage.getItem(i));
+    // calling all ids in the loop
+    console.log($("#"+i))
+    $("#"+i).val(localStorage.getItem(i));
+}
 
 
-
-// WHEN I click into a timeblock... THEN I can enter an event. WHEN I click the save button for that timeblock ...THEN the text for that event is saved in local storage
-// !!!need to establish local storage
-
-
-
-
-// WHEN I refresh the page ... // THEN the saved events persist
